90c4d1f68c578da3a5ef54219bfb9ea90290cba5
realistic-talha <talhayameen98@gmail.com>
2025-05-04 01:21:57 +0500

    Fix ThemeProviderProps import in components/theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,8 +1,8 @@
 "use client"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import type { ThemeProviderProps } from "next-themes"
+import type { PropsWithChildren } from "react"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: PropsWithChildren<{ [key: string]: any }>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
 
